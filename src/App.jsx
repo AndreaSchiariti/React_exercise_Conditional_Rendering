f1a62bdad18de86a5eeb403985147d5ab9837549
AndreaSchiariti <schiariti.andrea@gmail.com>
2024-06-11 16:14:50 +0200

    feat: exercise syle completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import { Hello } from "./Hello";
 import { Message } from "./Message";
+/*import { Message } from "./Message";*/
 import { Welcome } from "./Welcome";
 
 export function App() {
   return (
     <div>
       <Hello />
-      <Welcome name="Andrea" age="34" />
+      <Welcome age="16" name="Manuel"/>
     </div>
   );
 }
-/* 
-export function App() {
-  return (
-    <div>
-      <Hello />
-      <Hello />
-      <Message />
-      <Welcome name="Andrea" age="34" />
-    </div>
-  );
-}
-
-Renderizzerò 2 <h2> con un componente message, quindi 2 <p> all'interno, più un <p>
-esterno ai componenti per un totale di:
-2 <h2>
-3 <p>
-*/
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,15 +2,15 @@ import { Age } from "./Age";
 
 export function Welcome({ name = "dear User", age }) {
   return (
-    <div>
-      <p>
-        Welcome, <strong>{name}!</strong>
+    <div className="welcome">
+      <p> 
+        Welcome, <strong>{name}</strong>!
       </p>
       {age > 18 && <Age age={age} />}
       {age && <Age age={age} />}
       {age > 18 && age < 65 && <Age age={age} />}
       {age > 18 && age < 65 && name === "John" && <Age age={age} />}
-      {<Age age={age} />}
+      {/* {<Age age={age} />} */}
     </div>
   );
 }
